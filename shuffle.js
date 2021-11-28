eda66cf8b0686ed89676dc39eaf1d548be2a4acf
rryaboshtan <rrvworkroman@gmail.com>
2021-11-28 03:10:41 +0200

    base sort and search algoritms

--- shuffle.js
+++ shuffle.js
@@ -1,11 +1,17 @@
 const input = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
 const shuffle = function (arr) {
-    for (let i = arr.length - 1; i > 0; i--) {
-       
+   for (let i = arr.length - 1; i > 0; i--) {
+      let temp = arr[i];
+      let rand = Math.floor(Math.random() * (i + 1));
+
+      arr[i] = arr[rand];
+      arr[rand] = temp;
    }
+
+   return arr;
 };
 
-for (let i = 0; i < array.length; i++) {
+for (let i = 0; i < 10; i++) {
    console.log(shuffle(input).join(','));
 }
