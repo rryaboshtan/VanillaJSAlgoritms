4163659853377880e514517c0292f8e2d0ca2c62
rryaboshtan <rrvworkroman@gmail.com>
2021-11-28 23:52:51 +0200

    Binary tree

---
+++ baseAlgoritms/binaryTree.js
@@ -0,0 +1,61 @@
+class BinaryTree {
+   constructor() {
+      this.root = null;
+   }
+   add(value) {
+      if (!this.root) {
+         this.root = new TreeNode(value);
+      } else {
+         let node = this.root;
+         let newNode = new TreeNode(value);
+
+         while (node) {
+            if (value > node.value) {
+               if (!node.right) {
+                  node.right = newNode;
+                  break;
+               }
+               node = node.right;
+            } else {
+               if (!node.left) {
+                  node.left = newNode;
+                  break;
+               }
+               node = node.left;
+            }
+         }
+
+        //  if (value > node.value) {
+        //     node.right = newNode;
+        //  } else {
+        //     node.left = newNode;
+        //  }
+      }
+   }
+
+   print(root = this.root) {
+      if (!root) {
+         return;
+      }
+      console.log(root.value);
+      this.print(root.left);
+      this.print(root.right);
+   }
+}
+
+class TreeNode {
+   constructor(value) {
+      this.value = value;
+      this.left = null;
+      this.right = null;
+   }
+}
+
+const tree = new BinaryTree();
+tree.add(4);
+tree.add(2);
+tree.add(6);
+tree.add(5);
+tree.add(2);
+tree.add(1);
+tree.print();
