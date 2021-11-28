83a0bc1362525b85aad1ef8666431d1b0ba58b96
rryaboshtan <rrvworkroman@gmail.com>
2021-11-28 22:30:40 +0200

    tree walk done

--- baseAlgoritms/treewalk.js
+++ baseAlgoritms/treewalk.js
@@ -1,47 +1,82 @@
 const tree = [
-    {
-        v: 5,
-        c: [
-            {
-                v: 10,
-                c: [
-                    {
-                        v:11,
-                    }
-                ]
-            },
-            {
-                v: 7,
-                c: [
-                    {
-                        v: 5,
-                        c: [
-                            {
-                                v:1,
-                            }
-                        ]
-                    }
-                ]
-            }
-        ]
-    },
-    {
-        v: 5,
-        c: [
-            {
-                v:10,
-            },
-            {
-                v:15,
-            }
-        ]
-    }
-]
+   {
+      v: 5,
+      c: [
+         {
+            v: 10,
+            c: [
+               {
+                  v: 11,
+               },
+            ],
+         },
+         {
+            v: 7,
+            c: [
+               {
+                  v: 5,
+                  c: [
+                     {
+                        v: 1,
+                     },
+                  ],
+               },
+            ],
+         },
+      ],
+   },
+   {
+      v: 5,
+      c: [
+         {
+            v: 10,
+         },
+         {
+            v: 15,
+         },
+      ],
+   },
+];
 
-const recursive = (tree) => {
+const recursive = tree => {
+   let sum = 0;
 
-}
+   tree.forEach(node => {
+      sum += node.v;
+      if (!node.c) {
+         return node.v;
+      }
 
-const iteration = (tree) => {
-    
-}
+      sum += recursive(node.c);
+   });
+
+   return sum;
+};
+
+const iteration = tree => {
+   if (!tree.length) {
+      return 0;
+   }
+   let sum = 0;
+   let stack = [];
+   tree.forEach(node => stack.push(node));
+
+   while (stack.length) {
+      const node = stack.pop();
+      sum += node.v;
+      if (node.c) {
+         node.c.forEach(child => stack.push(child));
+      }
+   }
+
+   return sum;
+};
+// performance.mark('first');
+console.time('first');
+console.log(recursive(tree));
+console.timeEnd('first');
+// console.timeEnd('first');
+// performance.mark('second');
+console.time('second');
+console.log(iteration(tree));
+console.timeLog('second');
